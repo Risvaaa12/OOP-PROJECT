9d1bec23e5b6aa181f29ebde918291cd5e363fc6
Risvaaa <106639600+Risvaaa12@users.noreply.github.com>
2022-09-30 16:54:33 +0800

    Add files via upload

---
+++ Project Greenfoot/Alien/Hero.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the Hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(4);
+        if (isAtEdge()){
+        setLocation(0,getY());
+        }
+    }
+}
