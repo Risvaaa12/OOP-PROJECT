95a12ae3324dfba5e8e16f45cabb9c409bbf2a77
Risvaaa <106639600+Risvaaa12@users.noreply.github.com>
2022-10-10 16:05:16 +0800

    Add files via upload

---
+++ Project Greenfoot/Tugas/TugasPert2-Alien/Hero.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.io.File;
+
+/**
+ * Write a description of class Hero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the Hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("W"))
+            setLocation(getX(),getY()-3);
+        if (Greenfoot.isKeyDown("S"))
+            setLocation(getX(),getY()+3);
+            
+        if (Greenfoot.isKeyDown("A"))
+            setLocation(getX()-3,getY());
+        if (Greenfoot.isKeyDown("D"))
+            setLocation(getX()+3,getY());
+    }  
+}
