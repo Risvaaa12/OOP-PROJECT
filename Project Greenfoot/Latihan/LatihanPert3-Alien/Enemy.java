a214232150d40c31a46a76e819fc57daec2b7fc1
Risvaaa <106639600+Risvaaa12@users.noreply.github.com>
2022-10-10 16:45:51 +0800

    Add files via upload

---
+++ Project Greenfoot/Latihan/LatihanPert3-Alien/Enemy.java
@@ -0,0 +1,41 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemy extends Actor
+{
+    /**
+     * Act - do whatever the Enemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Enemy()
+    {
+        setRotation(180);
+    }
+    
+    public Enemy(int speed)
+    {
+        setRotation(180);
+        this.speed = speed;
+    }
+    
+    public void removeEnemy()
+    {
+        if(isAtEdge())
+        {
+            // menghapus object dari world ketika sudah di ujung
+            getWorld().removeObject(this); 
+        }
+    }
+    
+    public int speed = 2;
+    public void act()
+    {
+        move(speed);
+        removeEnemy();
+    }
+}
---
+++ Project Greenfoot/Latihan/LatihanPert3-Alien/Fire.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Fire here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fire extends Actor
+{
+    /**
+     * Act - do whatever the Fire wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        fireShoot();
+        removeFromWorld();
+    }
+    
+    public void fireShoot()
+    {
+        setLocation(getX()+3, getY());
+    }
+    
+    public void removeFromWorld()
+    {
+        Actor enemy = getOneIntersectingObject(Enemy.class);
+        if(enemy != null)
+        {
+             getWorld().removeObject(enemy);
+             getWorld().removeObject(this);
+        }
+        else if(isAtEdge())
+        {
+            getWorld().removeObject(this);
+        }
+    }
+
+}
